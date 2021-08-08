b8023e708bfb863f5023a7a982c243830fcd5908
FoxSGR <foxsgrno1@gmail.com>
2021-08-08 13:23:44 +0100

    refactor: transition to jwt

--- src/auth/authorizationChecker.ts
+++ src/auth/authorizationChecker.ts
@@ -1,34 +1,31 @@
-import { Action } from 'routing-controllers';
-import { Container } from 'typedi';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { Action, UnauthorizedError } from 'routing-controllers';
 import { Connection } from 'typeorm';
 
-import { Logger } from '../lib/logger';
-import { AuthService } from './AuthService';
+export function authorizationChecker(
+  connection: Connection
+): (action: Action, roles: any[]) => Promise<boolean> | boolean {
+  return async function innerAuthorizationChecker(action: Action, roles: string[]): Promise<boolean> {
+    const authHeader = action.request.get('Authorization');
+    if (!authHeader) {
+      const customError = new UnauthorizedError('Authorization header not provided');
+      action.next(customError);
+      return false;
+    }
 
-export function authorizationChecker(connection: Connection): (action: Action, roles: any[]) => Promise<boolean> | boolean {
-    const log = new Logger(__filename);
-    const authService = Container.get<AuthService>(AuthService);
-
-    return async function innerAuthorizationChecker(action: Action, roles: string[]): Promise<boolean> {
-        // here you can use request/response objects from action
-        // also if decorator defines roles it needs to access the action
-        // you can use them to provide granular access check
-        // checker must return either boolean (true or false)
-        // either promise that resolves a boolean value
-        const credentials = authService.parseBasicAuthFromRequest(action.request);
-
-        if (credentials === undefined) {
-            log.warn('No credentials given');
-            return false;
-        }
-
-        action.request.user = await authService.validateUser(credentials.username, credentials.password);
-        if (action.request.user === undefined) {
-            log.warn('Invalid credentials given');
-            return false;
-        }
-
-        log.info('Successfully checked credentials');
-        return true;
-    };
+    return new Promise((resolve) => {
+      try {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.JWT_SECRET, (jwtPayload: JwtPayload) => {
+          ['iat', 'exp'].forEach((keyToRemove) => delete jwtPayload[keyToRemove]);
+          action.request.jwtPayload = jwtPayload;
+          resolve(true);
+        });
+      } catch (err) {
+        const customError = new UnauthorizedError('Token generation error');
+        action.next(customError);
+        resolve(false);
+      }
+    });
+  };
 }
